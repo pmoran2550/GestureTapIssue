dbd2ba5e95984a7e58ab11d2a614f3cd89b7ee72
Pat Moran <pmoran@virtualincision.com>
2023-12-11 16:15:51 -0600

    Initial Checkin

---
+++ MauiAppTest/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiAppTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            await Shell.Current.DisplayAlert("", "Tap Worked!", "Ok");
+        }
+    }
+
+}
